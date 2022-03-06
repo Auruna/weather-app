78f6c5bd03a97378faf8126f712b94a47f91ceea
Auruna <101048647+Auruna@users.noreply.github.com>
2022-03-06 12:52:31 +0000

    Add files via upload

---
+++ wapp.js
@@ -0,0 +1,63 @@
+let now = new Date();
+
+let h4 = document.querySelector("h4");
+
+let date = now.getDate();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let year = now.getFullYear();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+
+h4.innerHTML = `📅${day} ${date} ${month} ${year}
+🕑${hours}:${minutes}`;
+
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-text-input");
+
+  let h1 = document.querySelector("h1");
+  if (searchInput.value) {
+    h1.innerHTML = `${searchInput.value}`;
+  } else {
+    h1.innerHTML = null;
+    alert("Please type a city");
+  }
+}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
+
+function showTemperature(response) {
+  let h2 = document.querySelector("h2");
+  let temperature = Math.round(response.temp);
+  h2.innerHTML = `${response.data.main.temp}`;
+  let apiKey = "7ff48749e920739931710c547a7d77b5";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(`${apiUrl}`).then(showTemperature);
+}
